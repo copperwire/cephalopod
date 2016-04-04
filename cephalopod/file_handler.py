ea5b2819d4ed07e630baaea5cede02ce73a13585
copperwire <solli.robert@gmail.com>
2016-04-04 17:45:38 +0200

    change to dict structure

--- cephalopod/file_handler.py
+++ cephalopod/file_handler.py
@@ -13,16 +13,18 @@ class file_handler:
 	runtime 
 
 	Class to get data from the SIMS output files. 
-	The data is collected in groups by the delimiters in the data file, e.g. **** DATA START*** contains all numerical data. 
-	Operations are then preformed to convert the data to human and machine readable formats. 
+	The data is collected in groups by the delimiters in the data file, e.g. **** DATA START*** 
+	contains all numerical data. Operations are then preformed to convert the data to human and 
+	machine readable formats. 
 
-	The class can easily be changed to take an arbitrary data file with a known delimiter between types of parameters. 
-	If there are several data sets, submit a list with strings denoting the start of the dataset and the corresponding
-	attribute of the class will be a dictionary with the denoted rows as keys with substance as the first element
-	and the x and y axis as the second and third element.  
+	The class can easily be changed to take an arbitrary data file with a known delimiter between
+	types of parameters. If there are several data sets, submit a list with strings denoting the 
+	start of the dataset and the corresponding attribute of the class will be a dictionary with 
+	the denoted rows as keys with substance as the first element and the x and y axis as the
+	second and third element.  
 	
-	The class is initialized with the plotting module, if you wish to only use the file decoding aspect please view the sample 
-	running in the docstring of the runtime method.
+	The class is initialized with the plotting module, if you wish to only use the file decoding 
+	aspect please view the sample running in the docstring of the runtime method.
 	"""
 
 	def __init__(self, filename):
@@ -32,16 +34,17 @@ class file_handler:
 
 	def file_iteration(self, delim = "***"):
 		"""
-		Walks through the supplied data file and stores all lines in string format. The data is saved
-		by assigning each set of rows to the corresponding delimiter. The delimiting value (by default \" *** \")
-		determines where the method separates the objects. Each part of the file can be accessed through 
+		Walks through the supplied data file and stores all lines in string format. The data is
+		saved by assigning each set of rows to the corresponding delimiter. The delimiting value 
+		(by default \" *** \") determines where the method separates the objects. Each part of the
+		file can be accessed through 
 
 		getattr(ClassInstance, File Partition)
 
 		where File Partition is the string directly following the delimiter. 
 
-		Method will be expanded to be more robust in taking delimiters with spaces adjacent to the string following the 
-		delimiter. 
+		Method will be expanded to be more robust in taking delimiters with spaces adjacent 
+		to the string following the delimiter. 
 
 		"""
 		self.has_iterated = True
@@ -68,9 +71,9 @@ class file_handler:
 
 				self.attribute_names.append(attribute_name)
 				try:
-					setattr(self, attribute_name, lines[index_of_data_type + 1: data_type_indices[i + 1 ] - 1]) 
+					a = setattr(self, attribute_name, lines[index_of_data_type + 1: data_type_indices[i + 1 ] - 1]) 
 				except IndexError: 
-					setattr(self, attribute_name, lines[index_of_data_type + 1: ])
+					a = setattr(self, attribute_name, lines[index_of_data_type + 1: ])
 
 		
 	def data_conversion(self, data_name = "DATA START", key_row= [2, 3], nonposy = True):
@@ -82,15 +85,16 @@ class file_handler:
 
 		Keyword arguments:
 		data_name (string) = string following delimiter before the data set.
-		key_row (list) = list with the rows in the data set which contains information about the data
+		key_row (list) = list with the rows in the data set which contains information about 
+		the data
 
-		returns list of datasets where each dictionary has the keys:
+		returns dictionary with subdictionaries on the form:
 
-		data = 2 x n nested list where data[0] contains all data points corresponding to the key \" x_unit \"
-		x_unit = string with physical unit of data[0]
-		y_unit = string with physical unit of data[1]
-		sample info = nested list containing sample id, original filename, sample code etc.
-		sample name = name of the element measured by the SIMS process  
+                                
+                    /->"Data"--> name -->("x", "y", "element", "x/y units")
+		attribute--|
+				    \->gen_info --> file_attribute(all general info about the sample not specific to element) 
+
 		"""
 		try:	
 			data_set = getattr(self, data_name)
@@ -140,46 +144,44 @@ class file_handler:
 
 		variables_per_substance = len(x[0])/len(self.substances)
 
-		self.list_of_datasets = []
+		gen_info = {}
+		data = {}
+
+		for attribute in self.attribute_names:
+			"""
+			Shold implement conversion to one string with float argument appended as dictionary,
+			maybe? 
+			"""
+			value = getattr(self, attribute)
+			value = [line.split(" ") for line in value]
+			gen_info[attribute] = value
 
 		for name, number in zip(self.substances, np.arange(0, len(x[0]), 2, dtype = int)):
-			y = {}
-
 			if u[0][number] < 1e-1:
 				units.pop(number)
 			else:
 				units.pop(number+1)
 
-			y["data"] = {"x": np.array(u[:,number]), "y": np.array(u[:,number + 1]), "element": [name for i in range(len(np.array(u[:,number + 1])))]}
-			y["x_unit"] = units[number]
-			y["y_unit"] = units[number + 1]
+			data[name] = {"x": np.array(u[:,number]), "y": np.array(u[:,number + 1]), 
+						"element": [name for i in range(len(np.array(u[:,number + 1])))],
+						"x_unit": units[number],
+						"y_unit": units[number + 1],
+						"sample_element": name}
 
+		y = dict(data = data, gen_info = gen_info)
+		setattr(self, name, y)
 
-			for attribute in self.attribute_names:
-				"""
-				Shold implement conversion to one string with float argument appended as dictionary, maybe? 
-				"""
-				value = getattr(self, attribute)
-				value = [line.split(" ") for line in value]
-				y[attribute] = value
-			
-			y["sample element"] = name
-
-			setattr(self, name, y)
-			self.list_of_datasets.append(getattr(self, name))
-
-		return self.list_of_datasets
+		return y
 
 
 	def runtime(self, delim = "***", data_name = "DATA START", key_row= [2, 3]):
 		"""
-		Runs the file iterator and data conversion and returns a touple of the names of the analyzed elements 
-		and dictionaries containing all data 
+		Runs the file iterator and data conversion and returns a touple of the names of the analyzed 
+		elements and dictionaries containing all data 
 		"""
 
-		self.file_iteration(delim)
-		x = self.data_conversion(data_name, key_row)
-
-		return (self.substances, x)
+		self.file_iteration(delim);
+		x = self.data_conversion(data_name, key_row);
+		return  x
 
 
