8ff8c2f891d50e4b375c78f75c80b00149f5ff2a
copperwire <solli.robert@gmail.com>
2016-05-20 16:27:27 +0200

    debugging update

--- cephalopod/plotting_module.py
+++ cephalopod/plotting_module.py
@@ -1,4 +1,4 @@
-from octopus import file_handler
+from cephalopod import file_handler, file_chooser 
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
@@ -6,57 +6,51 @@ import numpy as np
 
 class plotter:
 
-	def __init__(self, filename):
-		self.filename = filename
+	def __init__(self, sources):
 
-	def pathfinder(self):
-		"""Find full path to filename """
-
+		self.sources = sources
 
 	def plot_machine(self): 
+		sources = self.sources		
 
-		class_instance = file_handler(self.filename)
-		class_instance.file_iteration()
-		data_sets = class_instance.data_conversion()
-		names = getattr(class_instance, "substances") 
-
-		if len(names) > 2:			
-
-			host = host_subplot(111, axes_class = AA.Axes)
-			plt.subplots_adjust(right = 0.75)
-
-			par1 = host.twinx()
-			par2 = host.twinx()
-
-			host.set_yscale("log")
-			par1.set_yscale("log")
-			par2.set_yscale("log")
-
-			offset = 60 
-			new_fixed_axis = par2.get_grid_helper().new_fixed_axis
-			par2.axis["right"] = new_fixed_axis(loc="right",
-			                                    axes=par2,
-			                                    offset=(offset, 0))
-
-			par2.axis["right"].toggle(all = True)
-
-			host.set_xlabel(data_sets[0]["x_unit"])
-				
-			plotty_things = [host, par1, par2]
-
-			for data_set, name, things in zip(data_sets, names, plotty_things):
-				x_val = data_set["data"]["x"]
-				y_val = data_set["data"]["y"]
-
-				x_unit = data_set["x_unit"]
-				y_unit = data_set["y_unit"]
-
-				things.set_ylabel(y_unit)
-				things.plot(x_val, y_val, label = data_set["sample element"])
-				plt.legend()
-
-			plt.show()
+		host = host_subplot(111, axes_class = AA.Axes)
 		
+		plt.subplots_adjust(right = 0.75)
+		plt.xticks(fontsize = 16)
+		plt.yticks(fontsize = 16) 
+
+		host.set_yscale("log")
+
+		host.set_xlabel(sources[0].data["x_unit"], fontsize = 25)
+			
+		for data_set in sources:
+			x_val = data_set.data["x"]
+			y_val = data_set.data["y"]
+
+			x_unit = "$%s$" %data_set.data["x_unit"]
+			y_unit = "$%s$" %data_set.data["y_unit"]
+
+			host.set_ylabel(y_unit, fontsize  = 25)
+			host.plot(x_val, y_val, label = data_set.data["sample_element"])
+			plt.legend()
+
+			if y_unit != sources[0].data["y_unit"]:
+				par2 = host.twinx()
+				par2.set_yscale("log")
+				par2.set_ylabel(y_unit)
+
+				offset = 60 
+				new_fixed_axis = par2.get_grid_helper().new_fixed_axis
+				par2.axis["right"] = new_fixed_axis(loc="right",
+				                                    axes=par2,
+				                                    offset=(offset, 0))
+				par2.axis["right"].toggle(all = True)
+
+
+		plt.show()
+		
+
+		"""
 		else:
 
 			data_set = data_sets[0]
@@ -74,6 +68,7 @@ class plotter:
 			plt.ylabel(y_unit)
 			plt.legend()
 			plt.show()
+		"""
 
 
 
